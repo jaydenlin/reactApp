4a0ceae6502c50a5db4ca1ce2e2e6f8f11ba97af
JaydenLin <j5905653@gmail.com>
2014-11-09 23:42:23 +0800

    add data json

--- Components/commentBox.jsx
+++ Components/commentBox.jsx
@@ -1,12 +1,17 @@
 /**
  * @jsx React.DOM
  */
+var data = [
+  {author: "Pete Hunt", text: "This is one comment haha"},
+  {author: "Jordan Walke", text: "This is *another* comment haha"}
+]; 
+ 
 var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
         Hello, world! I am a CommentBox.
-        <CommentList />
+        <CommentList data={data}/>
         <CommentForm />
       </div>
     );
